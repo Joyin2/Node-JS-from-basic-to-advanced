9cc51e6cf7d5b89a18f2ef0ebb77e48cc197a3d0
Joyin2 <joyinlaskar2000@gmail.com>
2022-10-19 00:29:24 +0530

    a

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ const path = require("path");
 const dirPath = path.join(__dirname, "newFolder");
 
 fs.readdir(dirPath, (err, files) => {
-  console.log(files);
+  files.forEach((item) => {
+    console.log(item);
+  });
 });
-
-
-
 
 // for (i = 1; i < 11; i++) {
 //   fs.writeFileSync(dirPath + "/hello" + i + ".txt", "a simple text file");
