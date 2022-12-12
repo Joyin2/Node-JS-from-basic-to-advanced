653bd4a67bd1d4e201eb23e5da111a9d0b3603e1
Joyin2 <joyinlaskar2000@gmail.com>
2022-12-12 20:58:45 +0530

    validator

--- npm/index.js
+++ npm/index.js
@@ -1,7 +1,10 @@
 // Importing module
 const chalk = require("chalk");
-
+const validator = require("validator");
 // Printing the text
 console.log(chalk.red("aayush"));
 console.log(chalk.red.underline("aayush"));
 console.log(chalk.red.underline.bold("GFGndfhnbjdnfj"));
+
+const res = validator.isEmail("joyinlaskar@gmail.ar");
+console.log(res);
