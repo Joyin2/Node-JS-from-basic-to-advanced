8461472d5a9e5b1e48676505cec90bef6f95a18e
Joyin2 <joyinlaskar2000@gmail.com>
2022-12-12 20:35:31 +0530

    chalk 4

--- npm/index.js
+++ npm/index.js
@@ -1,1 +1,7 @@
-const requier
+// Importing module
+const chalk = require("chalk");
+
+// Printing the text
+console.log(chalk.red("aayush"));
+console.log(chalk.red.underline("aayush"));
+console.log(chalk.red.underline.bold("GFGndfhnbjdnfj"));
